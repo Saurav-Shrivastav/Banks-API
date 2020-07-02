e641db3aa8f00f14a716cf0ce578c6148601a2e0
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2020-07-03 00:40:25 +0530

    Threading added

--- banks/views.py
+++ banks/views.py
@@ -2,24 +2,14 @@ from django.shortcuts import render
 from django.views import View
 from django.http import JsonResponse
 import csv
+from threading import Thread
 from django.contrib import messages
 from django.shortcuts import HttpResponseRedirect, render
 from banks.models import Bank, Branch
 from banks.api.serializers import BranchSerializer
 
-
-# Create your views here.
-class ImportView(View):
-    def get(self, request):
-        return render(request, 'import.html')
-    
-    def post(self, request):
-        csv_file = request.FILES.get('csv_file')
-        decoded_file = csv_file.read().decode('utf-8').splitlines()
-        reader = csv.DictReader(decoded_file)
-        count = 0
-        # ifsc_list = list(Branch.objects.values_list('ifsc', flat=True))
-        for row in reader:
+def upload(reader):
+    for row in reader:
             bank_id = row.get('bank_id')
             ifsc = row.get('ifsc')
             branch = row.get('branch')
@@ -50,9 +40,21 @@ class ImportView(View):
 
             # print("No of Rows imported - {} - {} ".format(count, branch_defaults))
             
-            count += 1
 
-        messages.success(request, "{} rows imported.".format(count))
-            
+# Create your views here.
+class ImportView(View):
+    def get(self, request):
+        return render(request, 'import.html')
+    
+    def post(self, request):
+        csv_file = request.FILES.get('csv_file')
+        decoded_file = csv_file.read().decode('utf-8').splitlines()
+        reader = csv.DictReader(decoded_file)
+        count = 0
+        # ifsc_list = list(Branch.objects.values_list('ifsc', flat=True))
+
+        messages.success(request, "rows imported.")
+        thread = Thread(target=upload, kwargs={"reader": reader})
+        thread.start()
         return render(request, 'import.html')
         
