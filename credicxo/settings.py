01b2ec3ff1285be2815ebc2c229811718574d3d1
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2020-07-02 12:54:06 +0530

    Making the app ready for deployment

--- credicxo/settings.py
+++ credicxo/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$amvcu%y0op@6gz!&z*b(cl)g90t3jowju+p0rr7zrwaag673m'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','$amvcu%y0op@6gz!&z*b(cl)g90t3jowju+p0rr7zrwaag673m')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
