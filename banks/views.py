e727526040f9eb3f73bc99e622f815ac530ac1af
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2020-07-02 17:36:49 +0530

    method to import data pf branches added

--- banks/views.py
+++ banks/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
+import csv
+from django.contrib import messages
+from django.shortcuts import HttpResponseRedirect, render
+from banks.models import Bank, Branch
+from banks.api.serializers import BranchSerializer
+
 
 # Create your views here.
+class ImportView(View):
+    def get(self, request):
+        return render(request, 'import.html')
+    
+    def post(self, request):
+        csv_file = request.FILES.get('csv_file')
+        decoded_file = csv_file.read().decode('utf-8').splitlines()
+        reader = csv.DictReader(decoded_file)
+        count = 0
+        # ifsc_list = list(Branch.objects.values_list('ifsc', flat=True))
+        for row in reader:
+            bank_id = row.get('bank_id')
+            ifsc = row.get('ifsc')
+            branch = row.get('branch')
+            address = row.get('address')
+            city = row.get('city')
+            district = row.get('district')
+            state = row.get('state')
+            print("IFSC-- {}".format(ifsc))
+            if not ifsc:
+                break
+            bank_object, created = Bank.objects.get_or_create(
+                id=bank_id
+            )
+            branch_defaults = {
+                'branch': branch,
+                'bank_id': bank_object,
+                'address': address,
+                'city': city,
+                'district': district,
+                'state': state    
+            }
+        
+            branch_object, created = Branch.objects.update_or_create(
+                ifsc=ifsc, defaults=branch_defaults
+            )
+            if created:
+                print("row created{}".format(branch_defaults))
+
+            # print("No of Rows imported - {} - {} ".format(count, branch_defaults))
+            
+            count += 1
+
+        messages.success(request, "{} rows imported.".format(count))
+            
+        return render(request, 'import.html')
+        
--- credicxo/urls.py
+++ credicxo/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from banks.views import ImportView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/banks/', include('banks.api.urls'))
+    path('api/banks/', include('banks.api.urls')),
+    path('import/', ImportView.as_view(), name='import')
 ]
